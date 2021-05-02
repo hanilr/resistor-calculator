acbcd31eebdc8c9e44929b976ec245720764db84
Hakkı Anıl Ragıboğlu <77579421+hanilr@users.noreply.github.com>
2021-05-03 01:34:41 +0300

    Update 1.0

---
+++ pref_def.py
@@ -0,0 +1,63 @@
+# Screen Preferences #
+main_scr = "752x260"
+help_scr = "762x510"
+four_scr = "490x280"
+five_scr = "550x280"
+six_scr = "620x280"
+# ------------------ #
+
+# Standard Color Defines #
+hex_black = "#000000"
+hex_brown = "#6f4e37"
+hex_red = "#ff0000"
+hex_orange = "#ff8c00"
+hex_yellow = "#ffff00"
+hex_green = "#00ff00"
+hex_blue = "#0000ff"
+hex_purple = "#800080"
+hex_gray = "#606060"
+hex_white = "#ffffff"
+hex_gold = "#ffd700"
+hex_silver = "#c0c0c0"
+# ---------------------- #
+
+# Color Preferences #
+bg_canvas = "#898989" # Default First Canvas Color
+bg_menu = "#303030"
+bg_button = "#303030"
+bg_entry = "#303030"
+bg_color = "#393939"
+fg_color = "#e9e9e9"
+# ----------------- #
+
+# Menu Setup Config #
+list_one = [("Brown", hex_brown), ("Red", hex_red), ("Orange", hex_orange), ("Yellow", hex_yellow),
+            ("Green", hex_green), ("Blue", hex_blue), ("Purple", hex_purple),("Gray", hex_gray),
+            ("White", hex_white)]
+list_two = [("Black", hex_black), ("Brown", hex_brown), ("Red", hex_red), ("Orange", hex_orange), 
+            ("Yellow", hex_yellow), ("Green", hex_green), ("Blue", hex_blue), ("Purple", hex_purple),
+            ("Gray", hex_gray), ("White", hex_white)]
+list_multiplier = [("Black", hex_black), ("Brown", hex_brown), ("Red", hex_red), ("Orange", hex_orange), 
+                    ("Yellow", hex_yellow), ("Green", hex_green), ("Blue", hex_blue), ("Purple", hex_purple),
+                    ("Gray", hex_gray), ("White", hex_white), ("Gold", hex_gold), ("Silver", hex_silver)]
+list_tolerance = [("Brown", hex_brown), ("Red", hex_red), ("Orange", hex_orange), ("Yellow", hex_yellow),
+                ("Green", hex_green), ("Blue", hex_blue), ("Purple", hex_purple), ("Gray", hex_gray),
+                ("Gold", hex_gold), ("Silver", hex_silver)]
+list_temperature = [("Brown", hex_brown), ("Red", hex_red), ("Orange", hex_orange), ("Yellow", hex_yellow),
+                     ("Blue", hex_blue), ("Purple", hex_purple)]
+# ----------------- #
+
+# Color Value Variable #
+list_black = [0, 0, 0, 1, 1, 1]
+list_brown = [10, 1, 1, 10, 0.01, 100]
+list_red = [20, 2, 2, 100, 0.02, 50]
+list_orange = [30, 3, 3, 1000, 0.03, 15]
+list_yellow = [40, 4, 4, 10000, 0.04, 25]
+list_green = [50, 5, 5, 100000, 0.005, 1]
+list_blue = [60, 6, 6, 1000000, 0.0025, 10]
+list_purple = [70, 7, 7, 10000000, 0.0010, 5]
+list_gray = [80, 8, 8, 100000000, 0.0005, 1]
+list_white = [90, 9, 9, 1000000000, 1, 1]
+list_gold = [0, 0, 0, 0.1, 0.05, 1]
+list_silver = [0, 0, 0, 0.01, 0.10, 1]
+# -------------------- #
